4348023d8bceb5cf5875c8b3813b2a86dee612f4
wikswiks <wiktoriazuber1312@gmail.com>
2024-07-25 17:44:28 +0200

    2

--- tests/pulpite.spec.ts
+++ tests/pulpite.spec.ts
@@ -3,11 +3,15 @@ import { test, expect } from "@playwright/test";
 test.describe("User send money", () => {
   const userId = "kakaszek";
   const userPassword = "12345678";
+  const expectedUserName = "Jan Demobankowy";
+ 
   test.beforeEach(async ({ page }) => {
     const url = "https://demo-bank.vercel.app/"; 
     await page.goto(url);
-  
-  
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
+    
   })
   
   test("1. quick send with correct data", async ({ page }) => {
@@ -16,7 +20,7 @@ test.describe("User send money", () => {
    
     
     
-    const expectedUserName = "Jan Demobankowy";
+    
     const transferReceiver = "2";
     const transferAmount = "150";
     const transferTitle = "Pizza";
@@ -25,9 +29,9 @@ test.describe("User send money", () => {
     //Act
 
    
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userPassword);
-    await page.getByTestId("login-button").click();
+   
+    
+   
     await page
       .locator("#widget_1_transfer_receiver")
       .selectOption(transferReceiver);
@@ -51,16 +55,16 @@ test.describe("User send money", () => {
    
      
       
-      const expectedUserName = "Jan Demobankowy";
+      
       const topupReceiver = "500 xxx xxx";
       const topupAmount = "50";
   
       //Act
   
      
-      await page.getByTestId("login-input").fill(userId);
-      await page.getByTestId("password-input").fill(userPassword);
-      await page.getByTestId("login-button").click();
+      
+      
+      
       await page.locator("#widget_1_topup_receiver").selectOption(topupReceiver);
       await page.locator("#widget_1_topup_amount").fill(topupAmount);
       await page.locator("#uniform-widget_1_topup_agreement span").click();
